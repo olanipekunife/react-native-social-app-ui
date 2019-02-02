2efa301900c05a802dc6bbcba7836e3aaad01207
Olanipekun Ifeoluwa <olanipekunifeoluwao@gmail.com>
2019-02-02 18:11:28 +0100

    post done

--- components/Newphotopost.js
+++ components/Newphotopost.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Platform, View, StyleSheet, Image, KeyboardAvoidingView, Alert, ImageBackground, AsyncStorage, TouchableHighlight, TouchableOpacity, BackHandler, ToastAndroid,ActivityIndicator } from 'react-native';
+import { Platform, View, StyleSheet, Image, KeyboardAvoidingView, Alert, ImageBackground, AsyncStorage, TouchableHighlight, TouchableOpacity, BackHandler, ToastAndroid, ActivityIndicator } from 'react-native';
 import { Container, Header, Button, Item, Input, Icon, Label, Toast, Textarea, Footer, FooterTab, Left, Body, Thumbnail, Tabs, Tab, TabHeading } from 'native-base';
 import axios from 'axios';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -11,31 +11,61 @@ import { Text } from '../components/Text';
 import Ip from '../constants/Ip';
 
 export default class Newphotopost extends Component {
-  state = { media: null, load:false, mediatype:'', text:'' }
-  post=()=>{
-if(!this.state.media || !this.state.mediatype || !this.state.text){
- return ToastAndroid.show('Please what do you want to post about', ToastAndroid.LONG);
-}
-axios({
-  url: `http://${Ip.ip}:4001/post`,
-  method: 'post',
-  headers: {
-   Accept: 'application/json',
-   'Content-Type': 'multipart/form-data',
-  },
-  data: this.state
-}).then(({ data }) => {
-this.setState({ load:false});
-return ToastAndroid.show('Posted!', ToastAndroid.LONG);
-console.log(data);
-}).catch(err => {
-  console.log(JSON.stringify(err));
-});
+  state = { media: null, load: false, mediatype: '', text: '', uri: null, pic: 'https://via.placeholder.com/800', user: '' }
+ async componentDidMount() {
+    let user = await AsyncStorage.getItem('user');
+    user = JSON.parse(user);
+    this.setState({ pic: user.pic, user: user._id });
+  }
+  
+  post = () => {
+    if (!this.state.text || !this.state.uri) {
+      return ToastAndroid.show('Please what do you want to post about', ToastAndroid.LONG);
+    }
+    this.setState({ load: true });
+    const picdata = new FormData();
+    picdata.append('file', {
+      uri: this.state.uri,
+      type: `image/${this.state.uri.split('.').pop()}`,
+      name: `${this.state.uri.split('/').pop()}`
+    });
+    ToastAndroid.show('Uploading Image...', ToastAndroid.SHORT);
+    axios({
+      url: `http://${Ip.ip}:4001/uploadpic/post`,
+      method: 'post',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+      data: picdata
+    }).then(({ data }) => {
+       console.log(data);
+      if (!data.url) {
+        return ToastAndroid.show('Picture failed to upload', ToastAndroid.LONG);
+      }  
+      
+      this.setState({ media: data.url, mediatype: 'image' }, () => {
+        ToastAndroid.show('Sending your Post...', ToastAndroid.SHORT);
+
+            axios({
+        url: `http://${Ip.ip}:4001/post`,
+        method: 'post',
+        data: this.state
+      }).then(res => {
+        this.setState({ load: false }); 
+         console.log(res.data);
+        return ToastAndroid.show('Done!', ToastAndroid.LONG);
+      }).catch(err => {
+        console.log(err.response);
+      });
+      });
+    }).catch(err => {
+      console.log(err.response);
+    });
   }
   pickimage = async () => {
-  
-      this.setState({ load: true });
-       
+    this.setState({ load: true });
+
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: 'Images',
       allowsEditing: true,
@@ -44,57 +74,39 @@ console.log(data);
     if (result.cancelled) {
       ToastAndroid.show('You need to Pick an Image', ToastAndroid.LONG);
     } else {
-     // this.setState({ pic: result });
-      const picdata = new FormData();
-      picdata.append('file', {
-        uri: result.uri,
-        type: `image/${result.uri.split('.').pop()}`,
-        name: `${result.uri.split('/').pop()}`
-       });
-      axios({
-        url: `http://${Ip.ip}:4001/uploadpic/post`,
-        method: 'post',
-        headers: {
-         Accept: 'application/json',
-         'Content-Type': 'multipart/form-data',
-        },
-        data: picdata
-    }).then(({ data }) => {
-      this.setState({ media: data.url, load:false, mediatype:'image' });
-console.log(data);
-      }).catch(err => {
-        console.log(JSON.stringify(err));
-      });
-  //  }
-    console.log(result);
-  }
+      // this.setState({ pic: result });
+      this.setState({ uri: result.uri, load: false });
+      console.log(result);
+    }
   }
   render() {
     return (
       <ImageBackground
-        source={require('../assets/images/profile.jpeg')}
+        source={{ uri: this.state.pic }}
         style={[styles.container, { paddingTop: 20 }]}
       >
         <View style={styles.overlay} />
-        <Textarea placeholderTextColor='#fff' maxLength={80} style={{ color: '#fff', fontFamily: 'gibson', fontSize: 18 }} selectionColor='#fff' rowSpan={5}  onChangeText={(text) => this.setState({ text })} placeholder="What do you want to talk about?" />
+        <Textarea placeholderTextColor='#fff' maxLength={80} style={{ color: '#fff', fontFamily: 'gibson', fontSize: 18 }} selectionColor='#fff' rowSpan={5} onChangeText={(text) => this.setState({ text })} placeholder="What do you want to talk about?" />
         <View style={{ flex: 1, justifyContent: 'space-between', marginBottom: 15 }}>
-          <View style={{ width: Layout.window.width, flex: 0.8 }}>    
-          <Image style={{ flex: 1 }} resizeMode='cover' source={{ uri: this.state.media }} /></View>
+          <View style={{ width: Layout.window.width, flex: 0.8 }}>
+            <Image style={{ flex: 1 }} resizeMode='contain' source={{ uri: this.state.uri }} /></View>
 
-          <Button iconLeft full onPress={() => this.post()} light style={{ flex: 0.15 }}>
+            {this.state.load ? <Button full light style={{ flex: 0.15 }}>
+              <ActivityIndicator style={{ alignSelf: 'center', textAlign: 'center' }} size="small" color='#000' />
+            </Button> : <Button iconLeft full onPress={() => this.post()} light style={{ flex: 0.15 }}>
             <Micon name='plus-circle-outline' color='rgb(85,116,247)' />
             <Text style={{ color: 'rgb(85,116,247)' }}>POST</Text>
-          </Button>
+          </Button>}
         </View>
         <Footer style={{}}>
           <FooterTab style={{ backgroundColor: Colors.sky }}>
-          {this.state.load ? <Button>
-              <ActivityIndicator style={{ alignSelf: 'center', textAlign: 'center' }} size="small" color={Colors.sky2} />
-            </Button> :  <Button vertical onPress={this.pickimage}>
-              <Micon name='camera' color='#fff' />
-              <Text style={{ color: '#fff' }}>Upload a Photo</Text>
-            </Button>}   
-           
+            {this.state.load ? <Button>
+              <ActivityIndicator style={{ alignSelf: 'center', textAlign: 'center' }} size="small" color='#fff' />
+            </Button> : <Button vertical onPress={this.pickimage}>
+                <Micon name='camera' color='#fff' />
+                <Text style={{ color: '#fff' }}>Upload a Photo</Text>
+              </Button>}
+
           </FooterTab>
         </Footer>
       </ImageBackground>
